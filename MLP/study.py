58093d0059da3bf6fb821335530abe8f775af140
doubibobo <doubibobolalala@gmail.com>
2020-08-04 17:35:54 +0800

    ouput the proportion of different labels in train data and test data

--- MLP/study.py
+++ MLP/study.py
@@ -8,13 +8,13 @@ from data.code.tools.network_tools import train_process as tp
 from data.code.tools.training_tools import gpu_selector as gs
 
 K = 10                      # 进行10折交叉验证
-EPOCH_NUMBER = 1000         # 循环迭代次数为20
+EPOCH_NUMBER = 100          # 循环迭代次数为20
 LEARNING_RATE = 0.001        # 学习率
 WEIGHT_DELAY = 0
 BATCH_SIZE = 32
 INPUT_SIZE = 38
 OUTPUT_SIZE = 6
-FILE_NAME = "MLP_network_bs_32_epoch_1000_lr_small_bn_01.pkl"
+FILE_NAME = "MLP_network_bs_32_epoch_1000_lr_small_bn_02.pkl"
 
 
 if __name__ == '__main__':
@@ -42,11 +42,11 @@ if __name__ == '__main__':
     tp.train_process(torch_data, torch_label, network, K, LEARNING_RATE, EPOCH_NUMBER, BATCH_SIZE,
                      network_filename=FILE_NAME, gpu_available=gpu_available)
     # 不进行训练集合验证集合的划分，将全部数据拿来训练
-    final_network = net.Network(INPUT_SIZE, OUTPUT_SIZE)
-    tp.train_final_network(final_network, torch_data.float(), torch_label, LEARNING_RATE, EPOCH_NUMBER, WEIGHT_DELAY,
-                           BATCH_SIZE, FILE_NAME)
-
-    # 进行测试集验证
-    test_data, _ = da.csv_handle("../data/test_extend.csv")
-    fe.write_result_to_csv("../data/test_extend.csv", "models/result-07.csv",
-                           tp.test_process(test_data, FILE_NAME, gpu_available))
+    # final_network = net.Network(INPUT_SIZE, OUTPUT_SIZE)
+    # tp.train_final_network(final_network, torch_data.float(), torch_label, LEARNING_RATE, EPOCH_NUMBER, WEIGHT_DELAY,
+    #                        BATCH_SIZE, FILE_NAME, gpu_available)
+    #
+    # # 进行测试集验证
+    # test_data, _ = da.csv_handle("../data/test_extend.csv")
+    # fe.write_result_to_csv("../data/test_extend.csv", "models/result-07.csv",
+    #                        tp.test_process(test_data, FILE_NAME, gpu_available))
