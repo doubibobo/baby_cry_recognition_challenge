c3ad224f7c8009e55fb4c0ac599b8edc5cb27636
doubibobo <15071267961@163.com>
2020-08-04 19:16:07 +0800

    local commit

--- tools/network_tools/log_rmse.py
+++ tools/network_tools/log_rmse.py
@@ -1,4 +1,5 @@
 import torch
+from data.code.tools.training_tools import statistics_counter as sc
 
 
 def log_rmse(flag, network, x, y, loss_function):
@@ -17,6 +18,10 @@ def log_rmse(flag, network, x, y, loss_function):
     output = network(x)
     result = torch.max(output, 1)[1].view(y.size())  # 只返回最大值的每个索引
     corrects = (result.data == y.data).sum().item()
+
+    # 计算每个类别的准确率
+    if flag:
+        sc.counter_statistics(result.data)
 
     accuracy = corrects * 100 / len(y)
     loss = loss_function(output, y)
--- tools/training_tools/statistics_counter.py
+++ tools/training_tools/statistics_counter.py
@@ -4,15 +4,17 @@ from collections import Counter
 def counter_statistics(waiting):
     """
     统计List中每个元素的出现次数
+    :param waiting: 待处理的list
+    :return: 无返回
     """
     value = waiting.numpy()
     result = Counter(value.tolist())
     summary = sum(result.values())
     result = result.items()
 
-    accuracy = []
+    proportion = []
 
     for key, v in result:
-        accuracy.append((key, v / summary))
-    print(result)
-    print(accuracy)
+        proportion.append((key, v / summary))
+    print(sorted(result))
+    print(sorted(proportion))
